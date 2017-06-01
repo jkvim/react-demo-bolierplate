00bcd992d9b9ac79fba1833f7e688e13c63641a7
jkvim <jkvim@outlook.com>
2017-06-01 15:30:43 +0800

    Remove react.

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import './index.html';
 import './index.scss';
-
-if (process.env.NODE_ENV !== 'production') {
-  require('./index.html'); // eslint-disable-line global-require
-}
-
-/*
-  import your component and replace <App />
-*/
-
-function App() {
-  return (
-    <div>Hello Genius</div>
-  );
-}
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
