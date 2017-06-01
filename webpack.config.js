188f9f8aa679ae96bff1ad378d56ecf8e9f93dcd
jkvim <jkvim@outlook.com>
2017-06-01 16:18:31 +0800

    Remove CSS Module in webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
         test: /\.scss$/,
         use: [
           'style-loader',
-          'css-loader?importLoaders=1&modules&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+          'css-loader',
           'sass-loader',
         ],
       },
